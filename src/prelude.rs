604a7a2a64005794c00e1d2222ae53e279733c6c
Tamme Schichler <tamme@schichler.dev>
2021-09-10 13:41:33 +0200

    Added a few more methods

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,5 @@
-use std::{borrow::Borrow, pin::Pin};
+use std::{borrow::Borrow, cell::Cell, convert::Infallible, pin::Pin};
+use tap::Pipe;
 
 pub trait RefPineMap<K: Ord, V> {
 	/// Returns a reference to the value corresponding to the key.
@@ -22,6 +23,31 @@ pub trait RefPineMap<K: Ord, V> {
 		key: K,
 		value_factory: F,
 	) -> Result<Result<&V, (K, F)>, E>;
+
+	/// Inserts a new value produced by the given factory, but only if no such key exists yet.
+	///
+	/// # Errors
+	///
+	/// Iff an entry matching `key` already exists.
+	fn insert_with<F: FnOnce() -> V>(&self, key: K, value_factory: F) -> Result<&V, (K, F)> {
+		let value_factory = Cell::new(Some(value_factory));
+		self.try_insert_with::<_, Infallible>(key, || {
+			value_factory.take().expect("unreachable")().pipe(Ok)
+		})
+		.expect("unreachable")
+		.map_err(|(k, _)| (k, value_factory.take().expect("unreachable")))
+	}
+
+	/// Inserts a new value, but only if no such key exists yet.
+	///
+	/// # Errors
+	///
+	/// Iff an entry matching `key` already exists.
+	fn insert(&self, key: K, value: V) -> Result<&V, (K, V)> {
+		let value = Cell::new(Some(value));
+		self.insert_with(key, || value.take().expect("unreachable"))
+			.map_err(|(k, _)| (k, value.take().expect("unreachable")))
+	}
 }
 
 pub trait MutPineMap<K: Ord, V> {
@@ -53,6 +79,35 @@ pub trait MutPineMap<K: Ord, V> {
 		key: K,
 		value_factory: F,
 	) -> Result<Result<&mut V, (K, F)>, E>;
+
+	/// Inserts a new value produced by the given factory, but only if no such key exists yet.
+	///
+	/// # Errors
+	///
+	/// Iff an entry matching `key` already exists.
+	fn insert_with<F: FnOnce() -> V>(
+		&mut self,
+		key: K,
+		value_factory: F,
+	) -> Result<&mut V, (K, F)> {
+		let value_factory = Cell::new(Some(value_factory));
+		self.try_insert_with::<_, Infallible>(key, || {
+			value_factory.take().expect("unreachable")().pipe(Ok)
+		})
+		.expect("unreachable")
+		.map_err(|(k, _)| (k, value_factory.take().expect("unreachable")))
+	}
+
+	/// Inserts a new value, but only if no such key exists yet.
+	///
+	/// # Errors
+	///
+	/// Iff an entry matching `key` already exists.
+	fn insert(&mut self, key: K, value: V) -> Result<&mut V, (K, V)> {
+		let value = Cell::new(Some(value));
+		self.insert_with(key, || value.take().expect("unreachable"))
+			.map_err(|(k, _)| (k, value.take().expect("unreachable")))
+	}
 }
 
 /// # Safety
@@ -92,6 +147,35 @@ pub unsafe trait PinRefPineMap<K: Ord, V>: RefPineMap<K, V> {
 			<Self as RefPineMap<_, _>>::try_insert_with(&*self, key, value_factory)
 				.map(|inner| inner.map(|value| Pin::new_unchecked(&*(value as *const _))))
 		}
+	}
+
+	/// Inserts a new value produced by the given factory, but only if no such key exists yet.
+	///
+	/// # Errors
+	///
+	/// Iff an entry matching `key` already exists.
+	fn insert_with<F: FnOnce() -> V>(
+		self: Pin<&Self>,
+		key: K,
+		value_factory: F,
+	) -> Result<Pin<&V>, (K, F)> {
+		let value_factory = Cell::new(Some(value_factory));
+		self.try_insert_with::<_, Infallible>(key, || {
+			value_factory.take().expect("unreachable")().pipe(Ok)
+		})
+		.expect("unreachable")
+		.map_err(|(k, _)| (k, value_factory.take().expect("unreachable")))
+	}
+
+	/// Inserts a new value, but only if no such key exists yet.
+	///
+	/// # Errors
+	///
+	/// Iff an entry matching `key` already exists.
+	fn insert(self: Pin<&Self>, key: K, value: V) -> Result<Pin<&V>, (K, V)> {
+		let value = Cell::new(Some(value));
+		self.insert_with(key, || value.take().expect("unreachable"))
+			.map_err(|(k, _)| (k, value.take().expect("unreachable")))
 	}
 }
 
@@ -145,5 +229,34 @@ pub unsafe trait PinMutPineMap<K: Ord, V>: MutPineMap<K, V> {
 			)
 			.map(|inner| inner.map(|value| Pin::new_unchecked(&mut *(value as *mut _))))
 		}
+	}
+
+	/// Inserts a new value produced by the given factory, but only if no such key exists yet.
+	///
+	/// # Errors
+	///
+	/// Iff an entry matching `key` already exists.
+	fn insert_with<F: FnOnce() -> V>(
+		self: Pin<&mut Self>,
+		key: K,
+		value_factory: F,
+	) -> Result<Pin<&mut V>, (K, F)> {
+		let value_factory = Cell::new(Some(value_factory));
+		self.try_insert_with::<_, Infallible>(key, || {
+			value_factory.take().expect("unreachable")().pipe(Ok)
+		})
+		.expect("unreachable")
+		.map_err(|(k, _)| (k, value_factory.take().expect("unreachable")))
+	}
+
+	/// Inserts a new value, but only if no such key exists yet.
+	///
+	/// # Errors
+	///
+	/// Iff an entry matching `key` already exists.
+	fn insert(self: Pin<&mut Self>, key: K, value: V) -> Result<Pin<&mut V>, (K, V)> {
+		let value = Cell::new(Some(value));
+		self.insert_with(key, || value.take().expect("unreachable"))
+			.map_err(|(k, _)| (k, value.take().expect("unreachable")))
 	}
 }
